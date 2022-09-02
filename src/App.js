f9504c6887d2920a8c2c72a088724be48abfaff0
pratik511 <pp05111999@gmail.com>
2022-09-02 17:25:45 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './component/layout/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h>Hello</h>
+    <div>
+      <Header />
     </div>
   );
 }
